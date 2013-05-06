2844ba0dd8471120892384c919ee2f511ae53728
sgar.wang <sgar.wang@splashtop.com>
2013-05-06 16:30:46 +0800

    Feature: First /refinery

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
 jacob
 =====
 
-issac Web page for GLC Taiwan located in Taipei city, Taiwan.  This site was developed using Ruby on Rails. We need volunteers. For anyone who are interested in learning, please contact me.  Contributor:  sgarwang@gmail.com
+Web page for GLC Taiwan located in Taipei city, Taiwan.
+
+This site was developed using Ruby on Rails.
+We need volunteers.
+For anyone who are interested in learning, please contact me.
+
+Contributor:
+  * sgarwang@gmail.com
+
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jacob::Application.config.secret_token = 'a40019eb44c361b2470d7131cfdbb8e43ffb41e744514ee1a76fee2c16fb2934e75f516e22031831e1b0330b3c09650c05242a732a36b55dffd67aec5896c27f'
